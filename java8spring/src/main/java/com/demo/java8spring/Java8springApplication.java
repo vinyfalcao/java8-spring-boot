026ceff898e0efe8912c0f8de5172e8d6b441a48
Falcao <vinicius.falcao@accenture.com>
2018-06-01 11:28:03 -0300

    first commit

---
+++ java8spring/src/main/java/com/demo/java8spring/Java8springApplication.java
@@ -0,0 +1,12 @@
+package com.demo.java8spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8springApplication.class, args);
+	}
+}
---
+++ java8spring/src/test/java/com/demo/java8spring/Java8springApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.java8spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8springApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
